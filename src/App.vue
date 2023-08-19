c3e77f5d0f9a4577d5bd8b1e1241294645a363ba
Tobe0504 <ezimorahtobenna@gmail.com>
2023-08-19 02:55:40 +0100

    added initial/done files

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <Teleport to="#modal" v-if="showModal">
+    <Modal theme="sales" @close="toggleModal">
+      <h1>{{ header }}</h1>
+      <p>{{ content }}</p>
+    </Modal>
+  </Teleport>
+  <button @click="toggleModal">Show modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue App",
+      header: "Prepare for the giveaway",
+      content: "This is Tobe's Vue App",
+      showModal: false,
+    };
+  },
+  methods: {
+    buttonClickHandler() {
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+#modal {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+body {
+  margin: 0;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
